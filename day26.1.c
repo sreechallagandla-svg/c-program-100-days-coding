708e470c8ae17927a49a28eb4c32083500faeb14
sreechallagandla-svg <sreechallagandla@gmail.com>
2026-09-07 18:37:57 +0530

    Add files via upload

---
+++ day26.1.c
@@ -0,0 +1,21 @@
+//Q51: Write a program to print the following pattern:
+/*
+    5
+   45
+  345
+ 2345
+12345
+*/
+#include<stdio.h>
+int main() {
+    int i,j;
+    for(i=1;i<=5;i++){
+        for(j=1;j<=5-i;j++){
+            printf(" ");
+        }
+        for(j=5-i+1;j<=5;j++){
+            printf("%d",j);
+        }
+    }
+    return 0;
+}
